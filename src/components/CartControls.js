9a2ea63c94e20441bb843acca9acab8d52ed2b97
Salim Lati <niktonick@gmail.com>
2022-03-13 11:19:57 +0100

    hide order button when zero items

--- src/components/CartControls.js
+++ src/components/CartControls.js
@@ -1,12 +1,18 @@
 import styles from "./CartControls.module.css";
+import { useContext } from "react";
+import CartContext from "../context/CartContext";
 
-const CartControls = ({ onClose }) => (
-  <section className={styles["cart-controls"]}>
-    <button className={styles.close} onClick={onClose}>
-      Close
-    </button>
-    <button className={styles.order}>Order</button>
-  </section>
-);
+const CartControls = ({ onClose }) => {
+  const { items } = useContext(CartContext);
+  const showOrderButton = items.length > 0;
+  return (
+    <section className={styles["cart-controls"]}>
+      <button className={styles.close} onClick={onClose}>
+        Close
+      </button>
+      {showOrderButton && <button className={styles.order}>Order</button>}
+    </section>
+  );
+};
 
 export default CartControls;
